2b594a0f3a16b3a02b89f9f6addb3a2210afba56
Aniket Patil <67572642+aniketsonupatil@users.noreply.github.com>
2020-09-25 09:18:11 +0530

    Commit all for FirstSite GitHub Pages

---
+++ js/move.js
@@ -0,0 +1,23 @@
+
+	/*var menuBtn=document.getElementById("ham");
+	var sideNav=document.getElementById("sidenav");
+
+	sideNav.style.right="-250px";
+
+	menuBtn.onclick = function(){
+
+		if(sidenav.style.right=="-250px"){
+			sidenav.style.right="0";
+		}
+
+		else{
+			sidenav.style.right="-250px";
+		}
+	}*/
+
+
+	function donav(){
+		var element=document.getElementById("sidenav")
+		element.classList.toggle("inside");
+	}
+	
